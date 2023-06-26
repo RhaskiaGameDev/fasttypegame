c4c0cd8951c8f2be00ddf0f87995f87cbc35fa13
Rhaskia <94507929+RhaskiaGameDev@users.noreply.github.com>
2023-06-26 21:56:16 +0000

    partial switch to yew

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,66 @@
-mod checkanswer;
-mod csv;
-use crate::csv::*;
-
-
 #[macro_use] extern crate rocket;
-use rocket::serde::json::Json;
-use rocket::serde::json::json;
-use rocket::serde::json::Value;
-use rocket::fs;
+
+#[cfg(test)] mod tests;
+
+use rocket::{State, Shutdown};
 use rocket::fs::{relative, FileServer};
-use rocket::serde::{Deserialize, Serialize};
+use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
 }
 
-#[derive(Debug)]
-#[derive(Serialize, Deserialize)]
-struct Message<'r> {
-    contents: &'r str,
-}
+/// Returns an infinite stream of server-sent events. Each event is a message
+/// pulled from a broadcast queue sent by the `post` handler.
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
 
-#[derive(Serialize, Deserialize)]
-struct CurrLength (usize);
-
-#[post("/message", data = "<json>")]
-fn update(json: Json<Message>) -> String  {
-    let answers = &return_answers("worldcities.csv");
-
-    match in_answers(json.contents, &answers.0)
-    {
-        Some(a) => serde_json::to_string(&CurrLength( answers.1 )).unwrap(),
-        None =>  serde_json::to_string(&CurrLength( 0 )).unwrap(),
+            yield Event::json(&msg);
+        }
     }
+}
+
+/// Receive a message from a form submission and broadcast it to any receivers.
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
+}
+
+/// Receive a message from a form submission and broadcast it to any receivers.
+#[post("/message-check", data = "<form>")]
+fn post_check(form: Form<Message>) {
+    println!("{}", form.into_inner().message);
+
+
 }
 
 #[launch]
 fn rocket() -> _ {
-    println!("{:?}", clean_file("worldcities.csv", 0));
-
     rocket::build()
-        .mount("/", routes![update])
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events, post_check])
         .mount("/", FileServer::from(relative!("static")))
 }
